3dc540d02decfe4faed356cb5bb6cb189f462eb9
Simon Gartner <simon.gartner2003@gmail.com>
2025-08-22 17:09:55 +0200

    Removed unneccessary producers

--- demo/src/test/java/org/jboss/examples/ticketmonster/test/TicketMonsterDeployment.java
+++ demo/src/test/java/org/jboss/examples/ticketmonster/test/TicketMonsterDeployment.java
@@ -1,6 +1,5 @@
 package org.jboss.examples.ticketmonster.test;
 
-import org.jboss.examples.ticketmonster.util.Resources;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -10,7 +9,6 @@ public class TicketMonsterDeployment {
     public static WebArchive deployment() {
         return ShrinkWrap
                 .create(WebArchive.class, "test.war")
-                .addPackage(Resources.class.getPackage())
                 .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
                 .addAsResource("import.sql")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
