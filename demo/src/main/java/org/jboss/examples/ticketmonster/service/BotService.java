193be3f65f17312996fb5f73220d4841d24d9a65
Aleksandar Zivkovic <aleksandar.zivkovic@gepardec.com>
2025-08-22 13:35:55 +0200

    Add migrate Singleton from ejb to cdi

--- demo/src/main/java/org/jboss/examples/ticketmonster/service/BotService.java
+++ demo/src/main/java/org/jboss/examples/ticketmonster/service/BotService.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import jakarta.ejb.Asynchronous;
-import jakarta.ejb.Singleton;
 import jakarta.ejb.Timer;
 import jakarta.enterprise.event.Event;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.core.MultivaluedHashMap;
 
 import org.jboss.examples.ticketmonster.model.Booking;
